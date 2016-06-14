34c7f7750f4b7d3467ba25941612bdfe8ee25488
fang duan <df1228@gmail.com>
2016-06-14 14:25:04 +0800

    MISC

--- app/models/person.rb
+++ app/models/person.rb
@@ -5,6 +5,9 @@ class Person < ActiveRecord::Base
 
   accepts_nested_attributes_for :children, reject_if: :all_blank, allow_destroy: true
   mattr_accessor :relationships
+  belongs_to :province
+  belongs_to :city
+  belongs_to :district
 
   def full_address
     self.province
