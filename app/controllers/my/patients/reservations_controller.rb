1318e42be6165b92ead8396068983ad22dc602bb
fang duan <df1228@gmail.com>
2016-08-18 16:45:59 +0800

    FIX

--- app/controllers/my/patients/reservations_controller.rb
+++ app/controllers/my/patients/reservations_controller.rb
@@ -1,7 +1,7 @@
 require "rexml/document"
 
 class My::Patients::ReservationsController < InheritedResources::Base
-  before_action ->{ authenticate_user!( force: true ) }
+  before_action ->{ authenticate_user!( force: true ) }, :except => [:payment_notify]
   before_action :check_is_verified_doctor
   custom_actions :collection => [ :reservations, :status, :payment_notify, :payment_test ]
   before_action :deny_doctors, only: :show
