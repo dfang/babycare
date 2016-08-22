5f63053ddf49ebcbac5bb6d039bf7dad8dcbd3e8
fang duan <df1228@gmail.com>
2016-08-22 13:20:02 +0800

    MISC

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+	def underscore_body_class
+		body_class = params[:controller].underscore.gsub('/','_')
+		body_class += '_' + params[:action].underscore
+		body_class
+	end
 end
