661bed97c9209e628b9445735e14e8b279707eac
fang duan <df1228@gmail.com>
2016-09-07 10:59:20 +0800

    MISC

--- app/assets/javascripts/mobile/im.js
+++ app/assets/javascripts/mobile/im.js
@@ -15,14 +15,14 @@ var IM = {
 
 	call_support: function(caller, callee, reservation_id, callee_phone){
 			console.log("caller is " + caller, "callee is " + callee);
-
+      $('#loadingToast').show();
 			$.post('/background_jobs/call_support.json', {
 				caller: caller,
 				callee: callee,
 				reservation_id: reservation_id,
 				callee_phone: callee_phone
 			}, function(data){
-
+				$('#loadingToast').hide();
 			})
 	},
 
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,7 +13,7 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( admin.css admin.js )
 Rails.application.config.assets.precompile += %w( mobile.js mobile.css uploader.js)
 
-Rails.application.config.assets.precompile += %w( bootstrap_fontawesome.min )
+Rails.application.config.assets.precompile += %w( bootstrap_fontawesome )
 
 
 # Adding Webfonts to the Asset Pipeline
