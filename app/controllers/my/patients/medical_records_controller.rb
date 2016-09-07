8918c53b151eb6ee1ba478c74d515960a06751f2
fang duan <df1228@gmail.com>
2016-09-07 11:43:05 +0800

    MISC

--- app/controllers/my/patients/medical_records_controller.rb
+++ app/controllers/my/patients/medical_records_controller.rb
@@ -1,8 +1,21 @@
 class My::Patients::MedicalRecordsController < InheritedResources::Base
   before_action -> { authenticate_user!(force: true) }, except: []
+  before_action :find_reservation
 
   def index
     @medical_records = current_user.medical_records.order('created_at DESC')
+  end
+
+  def create
+    create! {
+      my_patients_reservation_path(@reservation)
+    }
+  end
+
+  def update
+    update! {
+      my_patients_reservation_path(@reservation)
+    }
   end
 
   def status
@@ -16,6 +29,11 @@ class My::Patients::MedicalRecordsController < InheritedResources::Base
   # def begin_of_association_chain
   #   current_user
   # end
+
+  def find_reservation
+    reservation_id ||= params[:reservation_id] || medical_record_params[:reservation_id]
+    @reservation ||= Reservation.find(reservation_id)
+  end
 
   def current_wechat_authentication
     current_user.authentications.where(provider: 'wechat').first
