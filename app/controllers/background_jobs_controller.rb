a61715fe1a680a81a7c9944a87037b1a0e441888
fang duan <df1228@gmail.com>
2016-09-21 14:50:27 +0800

    FIX

--- app/controllers/background_jobs_controller.rb
+++ app/controllers/background_jobs_controller.rb
@@ -1,57 +1,58 @@
 class BackgroundJobsController < ApplicationController
   skip_before_action :verify_authenticity_token
   respond_to :html, :json, :js
-	after_action :record_phone_call, only: :call
+  after_action :record_phone_call, only: :call
 
   def call
     @reservation = Reservation.find_by_id(params[:reservation_id])
-		if current_user.doctor.present?
-			# 医生给用户打电话
-			@reservation.user_b = current_user.doctor.id
-		else
-			# 用户给医生打电话
-			@reservation.user_b = params["callee"]
-		end
+    # 打电话不应该改变user_b的值
+    # if current_user.doctor.present?
+    #   # 医生给用户打电话
+    #   @reservation.user_b = current_user.doctor.id
+    # else
+    #   # 用户给医生打电话
+    #   @reservation.user_b = params["callee"]
+    # end
     @reservation.save!
 
     IM::Ronglian.call(params["caller"], params["callee"], params["reservation_id"], params['caller_phone'], params['callee_phone'])
   end
 
-	def call_support
-		IM::Ronglian.call(params["caller"], params["callee"], params["reservation_id"], params['callee_phone'])
-	end
+  def call_support
+    IM::Ronglian.call(params["caller"], params["callee"], params["reservation_id"], params['callee_phone'])
+  end
 
   def send_sms
 
   end
 
-	def cancel_reservation
-		reservation = Reservation.find_by(id: params[:reservation_id])
-		reservation.destroy!
-		render json: {
-			status: 'ok'
-		}
-	end
+  def cancel_reservation
+    reservation = Reservation.find_by(id: params[:reservation_id])
+    reservation.destroy!
+    render json: {
+      status: 'ok'
+    }
+  end
 
-	private
+  private
 
-	def record_phone_call
-		p "after phone call record it"
-		# {"caller"=>"1", "callee"=>"3", "reservation_id"=>"11", "reservation_phone"=>"17762575774", "controller"=>"background_jobs", "action"=>"call", "format"=>"json"}
+  def record_phone_call
+    p "after phone call record it"
+    # {"caller"=>"1", "callee"=>"3", "reservation_id"=>"11", "reservation_phone"=>"17762575774", "controller"=>"background_jobs", "action"=>"call", "format"=>"json"}
 
-		caller = User.find_by(id: params["caller"])
-		reservation = Reservation.find_by(id: params["reservation_id"])
-		reservation_state_when_call = reservation.aasm_state.to_s
+    caller = User.find_by(id: params["caller"])
+    reservation = Reservation.find_by(id: params["reservation_id"])
+    reservation_state_when_call = reservation.aasm_state.to_s
 
-		PhoneCallHistory.create(
-			caller_user_id: params["caller"],
-			caller_phone: caller.doctor.try(:mobile_phone),
-			callee_user_id: params["callee"],
-			callee_phone: params["reservation_phone"],
-			reservation_id: params["reservation_id"],
-			reservation_state_when_call: reservation_state_when_call
-		)
-	end
+    PhoneCallHistory.create(
+      caller_user_id: params["caller"],
+      caller_phone: caller.doctor.try(:mobile_phone),
+      callee_user_id: params["callee"],
+      callee_phone: params["reservation_phone"],
+      reservation_id: params["reservation_id"],
+      reservation_state_when_call: reservation_state_when_call
+    )
+  end
 
 
 end
