7c392721854e46ac94924880482ac8a4eed94054
fang duan <df1228@gmail.com>
2016-09-21 12:36:34 +0800

    MISC

--- test/factories/reservations.rb
+++ test/factories/reservations.rb
@@ -6,8 +6,12 @@ FactoryGirl.define do
     mobile_phone 17_762_575_774
     child_gender { %w(儿子 女儿).sample }
     remark { %w(咳嗽 发热 感冒 发高烧 拉肚子 水痘 幼儿急疹 过敏性皮炎 哮喘 鼻炎 支气管炎 肺炎).sample }
-    aasm_state :reserved
-    user_a { User.all.select { |x| x.doctor.nil? }.sample.id }
-    user_b { User.all.select { |x| x.doctor.present? }.sample.id }
+		aasm_state :pending
+		user_a { User.all.select { |x| x.doctor.nil? }.sample.id }
+
+		factory :reserved_reservations do
+			aasm_state :reserved
+			user_b { User.all.select { |x| x.doctor.present? }.sample.id }
+		end
   end
 end
