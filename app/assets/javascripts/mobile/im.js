1fc631165de25153e9ce8851e3fcf8e05e3a61a8
fang duan <df1228@gmail.com>
2016-09-21 13:29:27 +0800

    MISC

--- app/assets/javascripts/mobile/im.js
+++ app/assets/javascripts/mobile/im.js
@@ -1,28 +1,30 @@
 var IM = {
+  $.ajaxSetup({
+    beforeSend: function(jqXHR){
+      $('#loadingToast').show();
+    },
+    complete: function(jqXHR){
+      $('#loadingToast').hide();
+    }
+  })
   call: function(caller,  callee, reservation_id, callee_phone) {
 
       console.log("caller is " + caller, "callee is " + callee);
-      $('#loadingToast').show();
       $.post('/background_jobs/call.json', {
         caller: caller,
         callee: callee,
         reservation_id: reservation_id,
         callee_phone: callee_phone,
-      }, function(data){
-        $('#loadingToast').hide();
       })
   },
 
   call_support: function(caller, callee, reservation_id, callee_phone){
       console.log("caller is " + caller, "callee is " + callee);
-      $('#loadingToast').show();
       $.post('/background_jobs/call_support.json', {
         caller: caller,
         callee: callee,
         reservation_id: reservation_id,
         callee_phone: callee_phone
-      }, function(data){
-        $('#loadingToast').hide();
       })
   },
 
@@ -30,11 +32,10 @@ var IM = {
 
   },
 
-  cancel: function(){
+  cancel: function(reservation_id){
     $.post('/background_jobs/cancel_reservation.json', {
       reservation_id: reservation_id
     }, function(data){
-      $('#loadingToast').hide();
       window.location.href = '/my/patients/reservations'
     })
   }
