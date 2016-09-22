3354d2381a7610a75e220b1ac3b5842de70bcb3d
fang duan <df1228@gmail.com>
2016-09-22 11:07:34 +0800

    MISC

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -5,9 +5,6 @@ class RatingsController < InheritedResources::Base
 		ratings = Rating.where(reservation_id: params[:reservation_id], rated_by: params[:rated_by])
 		ratings.destroy_all if ratings.present?
 		create! {
-			resource.tag_list = rating_params[:body]
-			resource.save!
-
 			if current_user.is_verified_doctor?
 				my_doctors_reservation_path(parent)
 			else
