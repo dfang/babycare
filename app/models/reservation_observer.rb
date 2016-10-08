103a2ce4996ed4869ab493b885e298056c26fea9
fang duan <df1228@gmail.com>
2016-10-08 09:07:19 +0800

    MISC

--- app/models/reservation_observer.rb
+++ app/models/reservation_observer.rb
@@ -7,7 +7,7 @@ class ReservationObserver < ActiveRecord::Observer
 
   def after_create(reservation)
     # todo: delay job
-    Doctor.each do |doctor|
+    Doctor.find_each do |doctor|
       IM::Ronglian.send_templated_sms(doctor.mobile_phone, Settings.sms_templates.notify_all_when_new_reservation, resource.patient_user_name)
     end
   end
