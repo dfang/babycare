da60e2984533a687e9236de20773a7b42a8b2874
fang duan <df1228@gmail.com>
2016-10-11 14:02:00 +0800

    MISC

--- app/assets/javascripts/mobile/tab.js
+++ app/assets/javascripts/mobile/tab.js
@@ -1,3 +1,3 @@
 $('#container').on('click', '.weui-tabbar__item', function () {
-    $(this).addClass('weui_bar_item_on').siblings('.weui_bar_item_on').removeClass('weui_bar_item_on');
+    $(this).addClass('weui_bar_item__on').siblings('.weui_bar_item__on').removeClass('weui_bar_item__on');
 });
