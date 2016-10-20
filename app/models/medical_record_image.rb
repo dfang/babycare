54ff1227731488287500cc965824170d9719de33
fang duan <df1228@gmail.com>
2016-10-20 13:59:22 +0800

    MISC

--- app/models/medical_record_image.rb
+++ app/models/medical_record_image.rb
@@ -1,6 +1,6 @@
 class MedicalRecordImage < ActiveRecord::Base
-  # include ImageVersion
-  # mount_image_version :data
+  include ImageVersion
+  mount_image_version :data
 
   # mount_uploader :data, AdminImageUploader
   belongs_to :medical_record
