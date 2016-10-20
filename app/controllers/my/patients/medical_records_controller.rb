d336b6665b01ee97fe41d1ebeadcfe4c8b153c75
fang duan <df1228@gmail.com>
2016-10-20 13:08:48 +0800

    MISC

--- app/controllers/my/patients/medical_records_controller.rb
+++ app/controllers/my/patients/medical_records_controller.rb
@@ -47,7 +47,9 @@ class My::Patients::MedicalRecordsController < InheritedResources::Base
 
   def find_reservation
     reservation_id ||= params[:reservation_id] || medical_record_params[:reservation_id]
-    @reservation ||= Reservation.find(reservation_id)
+    if reservation_id.present?
+      @reservation ||= Reservation.find(reservation_id)
+    end
   end
 
   def current_wechat_authentication
