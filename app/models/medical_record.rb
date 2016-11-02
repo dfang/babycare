940477b2caff9b017a9fb3d81493d68e7d51067e
fang duan <df1228@gmail.com>
2016-11-02 17:23:49 +0800

    MISC

--- app/models/medical_record.rb
+++ app/models/medical_record.rb
@@ -11,8 +11,17 @@ class MedicalRecord < ActiveRecord::Base
   accepts_nested_attributes_for :imaging_examination_images, allow_destroy: true
 
   has_one :cover, class_name: 'MedicalRecordImage'
-	GENDERS = %w(男 女).freeze
+
+  GENDERS = %w(男 女).freeze
 	TEMPERATURES = %w(37 37.5 38 38.5 39 39.5 40 40.5 41 41.5 42).freeze
   HEIGHTS = (50..120).to_a.freeze
   WEIGHTS = (5.0..10).step(0.1).map { |x| x.round(2) }.freeze
+  PULSES = (45..15).to_a.freeze
+  RESPIRATORY_RATES = (60..120).to_a.freeze
+  # BLOOD_PRESSURES =  (190..30).to_a.freeze
+  # OXYGEN_SATURATIONS
+  BMI = (15..35).to_a.freeze
+  PAIN_SCORES = (0..10).to_a.freeze
+
+
 end
