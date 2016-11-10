1e29f653face1b9ce82fcf99e50f412a48d513f5
fang duan <df1228@gmail.com>
2016-11-10 13:54:18 +0800

    MISC

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,13 +2,23 @@ class Post < ActiveRecord::Base
   acts_as_taggable
 
   TAGS = %w(
-      3个月以下
-      3个月到一岁
-      一岁到三岁
-      三到六岁
-      六到十二岁
+      0到1岁
+      1岁到3岁
+      3到6岁
+      6到12岁
       给医生看的文章
       给患者看的文章
+      儿童外科
+      儿童耳鼻喉咽
+      儿童眼科
+      儿童口腔
+      儿童神经
+      儿童心血管
+      儿童内分泌
+      儿童血液
+      儿童消化
+      儿童呼吸
+      儿童保健
   )
 
 	scope :published, -> { where(published: true) }
