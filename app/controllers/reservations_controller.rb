af65c0807c9d46300c75bfd110b4a01bfa52d409
fang duan <df1228@gmail.com>
2016-11-17 00:17:15 +0800

    MISC

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -22,9 +22,9 @@ class ReservationsController < InheritedResources::Base
   end
 
   def new
-    @appId = Settings.wx_pay.app_id
-    @nonceStr = SecureRandom.hex
-    @timestamp =  DateTime.now.to_i
+    @appId      = Settings.wx_pay.app_id
+    @nonceStr   = SecureRandom.hex
+    @timestamp  =  DateTime.now.to_i
     js_sdk_signature_str = { jsapi_ticket: WxApp::WxCommon.get_jsapi_ticket, noncestr: @nonceStr, timestamp: @timestamp, url: request.url }.sort.map do |k,v|
                         "#{k}=#{v}" if v != "" && !v.nil?
                       end.compact.join('&')
--- app/controllers/wxpay_controller.rb
+++ app/controllers/wxpay_controller.rb
@@ -3,17 +3,26 @@ class WxpayController < ApplicationController
   respond_to :json, :js, :xml
 
   def config
-    appId = Settings.wx_pay.app_id
-    nonceStr = SecureRandom.hex
-    timestamp =  DateTime.now.to_i
-    js_sdk_signature_str = WxApp::WxPay.generate_js_sdk_signature_str(nonceStr, timestamp, params[:url])
-    signature = Digest::SHA1.hexdigest(js_sdk_signature_str)
+    appId                =  Settings.wx_pay.app_id
+    nonceStr             =  SecureRandom.hex
+    timestamp            =  DateTime.now.to_i
+    js_sdk_signature_str =  WxApp::WxPay.generate_js_sdk_signature_str(nonceStr, timestamp, params[:url])
+    signature            =  Digest::SHA1.hexdigest(js_sdk_signature_str)
+
+    # appId = Settings.wx_pay.app_id
+    # nonceStr = SecureRandom.hex
+    # timestamp =  DateTime.now.to_i
+    #
+    # js_sdk_signature_str = { jsapi_ticket: WxApp::WxCommon.get_jsapi_ticket, noncestr: nonceStr, timestamp: timestamp, url: params[:url] }.sort.map do |k,v|
+    #                     "#{k}=#{v}" if v != "" && !v.nil?
+    #                   end.compact.join('&')
+    # signature = Digest::SHA1.hexdigest(js_sdk_signature_str)
 
     render json: {
         appId: appId,
         key: Settings.wx_pay.key,
         mch_id: Settings.wx_pay.mch_id,
-        timestamp: timestamp,
+        timestamp: timestamp.to_s,
         nonceStr: nonceStr,
         signature: signature,
         jsApiList: ['checkJsApi', 'chooseWXPay', 'chooseImage', 'uploadImage', 'downloadImage', 'previewImage', 'openLocation', 'getLocation']
