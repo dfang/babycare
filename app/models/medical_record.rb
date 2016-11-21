b3aa941df733ad5b455521ab7f1d2d1dc95057b8
fang duan <df1228@gmail.com>
2016-11-21 10:11:41 +0800

    MISC

--- app/models/medical_record.rb
+++ app/models/medical_record.rb
@@ -15,7 +15,7 @@ class MedicalRecord < ActiveRecord::Base
   GENDERS = [['男', false], ['女', true]].freeze
 	TEMPERATURES = %w(37 37.5 38 38.5 39 39.5 40 40.5 41 41.5 42).map {|x| x.to_f }.freeze
   HEIGHTS = (50..120).to_a.freeze
-  WEIGHTS = (5.0..10).step(0.1).map { |x| x.round(2) }.freeze
+  WEIGHTS = (5.0..180).step(0.1).map { |x| x.round(2) }.freeze
   PULSES = (15..45).to_a.freeze
   RESPIRATORY_RATES = (60..120).to_a.freeze
   BLOOD_PRESSURES =  (30..190).to_a.freeze
