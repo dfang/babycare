2ea188227bc2aeb0db80a9c1004d2879b7944dad
fang duan <df1228@gmail.com>
2017-03-29 08:31:37 +0800

    MISC

--- app/models/medical_record_image.rb
+++ app/models/medical_record_image.rb
@@ -1,4 +1,7 @@
 class MedicalRecordImage < ActiveRecord::Base
+  establish_connection "odoo_#{Rails.env}"
+  self.table_name = 'fa_medical_record_image'
+
   include ImageVersion
   mount_image_version :data
 
