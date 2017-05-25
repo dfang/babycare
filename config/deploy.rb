db3c18ecb09e591ad10efd8dc59b34cdcc52152b
fang duan <df1228@gmail.com>
2017-05-25 23:20:10 +0800

    set deploy branch to rails branch

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,6 +7,8 @@ set :repo_url, 'git@git.coding.net:df1228/callmeadoctor.git'
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
+set :branch, "rails5"
+
 # Default deploy_to directory is /var/www/my_app
 set :deploy_to, '/home/deployer/apps/callmeadoctor'
 
