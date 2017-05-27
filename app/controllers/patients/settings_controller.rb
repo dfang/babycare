d88474b4c2f00fd241d22dcebf7b43a90fae0f44
fang duan <df1228@gmail.com>
2017-05-28 00:38:42 +0800

    MISC

--- app/controllers/patients/settings_controller.rb
+++ app/controllers/patients/settings_controller.rb
@@ -1,23 +1,6 @@
 class Patients::SettingsController < InheritedResources::Base
 
-  def new
-  end
-
   def edit
-  end
-
-  def show
-  end
-
-  def update
-    # if current_user.settings.present?
-    #   setting = current_user.settings.first
-    # else
-    #   setting = current_user.settings.build
-    # end
-    # setting.update(setting_params)
-    binding.pry
-    redirect_to patients_index_path and return
   end
 
   protected
