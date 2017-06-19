63a0a73ed2ed0fb45b7cd1f9d9aa4bb4e57e9a81
fang duan <df1228@gmail.com>
2017-06-19 18:59:08 +0800

    Maybe fix

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
+  before_action :authenticate_user!
   before_action :wechat_authorize
   # before_action :configure_sign_in_params, only: [:create]
   # prepend_before_action :authenticate_user!, :wechat_authorize
-  before_action :authenticate_user!
+
+  def new
+    redirect_to :wechat_authorize
+  end
 
   def wechat_authorize
     wx_authenticate!
