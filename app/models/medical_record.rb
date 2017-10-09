765f1f5b9a991e0f0739ee99b3723702c90bb900
havenwei <havenwei82@163.com>
2017-10-09 17:12:49 +0800

    debug user gender

--- app/models/medical_record.rb
+++ app/models/medical_record.rb
@@ -15,7 +15,7 @@ class MedicalRecord < OdooRecord
   accepts_nested_attributes_for :laboratory_examination_images, reject_if: :all_blank, allow_destroy: true
   accepts_nested_attributes_for :imaging_examination_images, reject_if: :all_blank, allow_destroy: true
 
-  GENDERS = [['男', false], ['女', true]].freeze
+  GENDERS = [['男', :male], ['女', :female]].freeze
   TEMPERATURES = %w[37 37.5 38 38.5 39 39.5 40 40.5 41 41.5 42].map(&:to_f).freeze
   HEIGHTS = (50..120).to_a.freeze
   WEIGHTS = (5.0..180).step(0.1).map { |x| x.round(2) }.freeze
