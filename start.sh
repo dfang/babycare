88e2cc29cc6c2e662f054cbe86333b1232d13f37
fang duan <df1228@gmail.com>
2017-10-09 09:30:12 +0800

    Add docker-compose.yml

---
+++ start.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+docker-compose down -v
+
+# docker volume create --name=odoo-web-data
+# docker volume create --name=odoo-db-data
+
+# 启动服务
+docker-compose up --build --force-recreate -d
+# docker-compose up -d
+
+# 初始化rails 数据库
+docker exec -ti -e DISABLE_DATABASE_ENVIRONMENT_CHECK=1 babycare bin/rails db:setup
+
+# 初始化odoo 数据库
+docker exec -ti odoo python /tmp/bootstrap_odoo_db.py
+
+# 查看babycare容器日志
+# docker logs -f babycare
