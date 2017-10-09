f47b4db46f30b558995778aeb1b6216a7e067045
fang duan <df1228@gmail.com>
2017-10-09 09:27:35 +0800

    Ignore actionpack variant

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,15 +1,15 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require jquery
+//= require mobile/jquery.min
+//= require mobile/parsley
 //= require jquery_ujs
+
+//= require mobile/bootstrap
+//= require mobile/router
+//= require mobile/im
+//= require mobile/tab
+//= require mobile/star-rating.min
+//= require mobile/bootstrap-tags
+
 //= require turbolinks
+//= require mobile/autosize
+
+//= require cable
