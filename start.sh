f70ca76280ea480d3f2022d810431d2e82850c71
fang duan <df1228@gmail.com>
2017-10-10 11:35:07 +0800

    Improve docker-compose.yml

--- start.sh
+++ start.sh
@@ -6,11 +6,11 @@ docker-compose down -v
 # docker volume create --name=odoo-db-data
 
 # 启动服务
-docker-compose up --build --force-recreate --scale app=3 -d
+docker-compose up --build --force-recreate -d
 # docker-compose up -d
 
 # 初始化rails 数据库
-docker exec -ti -e DISABLE_DATABASE_ENVIRONMENT_CHECK=1 babycare bin/rails db:setup
+docker exec -ti -e DISABLE_DATABASE_ENVIRONMENT_CHECK=1 babycare_app_1 bin/rails db:setup
 
 # 初始化odoo 数据库
 docker exec -ti odoo python /tmp/bootstrap_odoo_db.py
