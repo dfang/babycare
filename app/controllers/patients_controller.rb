676ecba4e7d07c2b6e91e03e9476df716d2c9e1d
fang duan <df1228@gmail.com>
2017-10-17 10:33:15 +0800

    Deny doctors on patients index page

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -2,6 +2,14 @@
 
 class PatientsController < InheritedResources::Base
   before_action -> { authenticate_user!(force: true) }
+  before_action :deny_doctors!
 
   def index; end
+
+  def deny_doctors!
+    if current_user.verified_doctor?
+      flash[:error] = '你是医生, 不能访问用户区域'
+      redirect_to global_denied_path and return
+    end
+  end
 end
