b8e34ca4448e28a7e12e75854ee5578339ffaf96
havenwei <havenwei82@163.com>
2017-10-25 11:42:02 +0800

    MISC

--- app/controllers/doctors/examinations_controller.rb
+++ app/controllers/doctors/examinations_controller.rb
@@ -1,28 +1,36 @@
 class Doctors::ExaminationsController < ApplicationController
   def new
     @examination_groups = ExaminationGroup.all
-    @reservation_examination = ReservationExamination.new
-    # @reservation = Reservation.find_by(id: params[:reservation_id])
+    @reservation = Reservation.find_by(id: params[:reservation_id])
+    @reservation_examinations = @reservation.reservation_examinations
+    if @reservation_examinations.present?
+      render :edit and return
+    else
+      @reservation_examination = ReservationExamination.new
+    end
 
-    p @reservation
+    # p @reservation
 
     # @reservation_examinations = @reservation.reservation_examinations.build()
   end
 
   def create
-    # Rails.logger.info params[:examination_id]
-    binding.pry
-
     reservation_id = params["reservation_id"]
     ids = params["reservation_examination"]["examination_id"].delete_if {|x| x.blank?}
     ids.each do |examination_id|
       ReservationExamination.create(reservation_id: reservation_id, examination_id: examination_id)
     end
 
-    binding.pry
-
     redirect_to doctors_reservation_path(reservation_id) and return
 
+  end
+
+  def edit
+    @reservation = Reservation.find_by(id: params[:reservation_id])
+    @reservation_examination = ReservationExamination.first
+    @reservation_examinations = @reservation.reservation_examinations
+
+    @examination_groups = ExaminationGroup.all
   end
 
   private
