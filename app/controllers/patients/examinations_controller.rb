1ff3ba49c0c7d2d44c79c86746f6c7271c9dc657
havenwei <havenwei82@163.com>
2017-11-21 11:49:28 +0800

    Patients can delete examination images in gallery. Display examinations in show page of reservation to consult.

--- app/controllers/patients/examinations_controller.rb
+++ app/controllers/patients/examinations_controller.rb
@@ -1,2 +1,9 @@
 class Patients::ExaminationsController < ApplicationController
+  def destroy
+    @reservation = current_user.reservations.find_by(id: params[:id])
+    @examination = @reservation.reservation_examinations.find_by(id: params[:reid])
+    @image = @examination.reservation_examination_images.find_by(id: params[:imgid])
+    @image.destroy if @image.present?
+    redirect_to examinations_uploader_patients_reservations_path(id: @reservation)
+  end
 end
