7728fdcf8843669bbbbff1caa60c9bf4d1814ed7
havenwei <havenwei82@163.com>
2017-11-21 17:56:17 +0800

    Fix instant preview when uploading in android

--- app/javascript/packs/examination_images_uploader.js
+++ app/javascript/packs/examination_images_uploader.js
@@ -1,11 +1,13 @@
 import $ from 'jquery';
 import wx from 'wechat-jssdk-promise';
+import util from './util.js';
 
 wx.ready( () => {
 
   console.log('document is ready');
   // console.log("isWeiXin: " + isWeiXin());
   // console.log("isAndroid: " + isAndroid());
+  // alert(util.isAndroid());
 
   // wx js sdk 图片上传 即时预览
   $(document).on('click', '.picker', (e) => {
@@ -31,22 +33,30 @@ wx.ready( () => {
     })
 
     const preview = ($files, localId) => {
-      wx.getLocalImgDataAsync({
-        localId: localId
-      }).then( (res) => {
-          let localData = res.localData;
-          let $li = $('<li class="weui-uploader__file">');
-          let $gallery = $('<div class="weui-gallery" style="display: none;">');
-          let $galleryImg = $('<span class="weui-gallery__img">');
-          let $galleryOpr = $('<div class="weui-gallery__opr"><i class="weui-icon-delete weui-icon_gallery-delete"></i></div>')
-          $li.css("background-image", "url(" + localData + ")");
-          $galleryImg.css("background-image", "url(" + localData + ")");
-          // $files.append($li);
-          $files.parents('.weui-uploader').find('.weui-uploader__files').append($li);
-          $li.append($gallery);
-          $gallery.append($galleryImg);
-          $gallery.append($galleryOpr);
-      })
+      if(util.isAndroid()){
+        appendToPlaceholder(localId);
+      }else{
+        wx.getLocalImgDataAsync({
+          localId: localId
+        }).then( (res) => {
+            let localData = res.localData;
+            appendToPlaceholder(localData);
+        })
+      }
+    }
+
+    const appendToPlaceholder = (img) => {
+      let $li = $('<li class="weui-uploader__file">');
+      let $gallery = $('<div class="weui-gallery" style="display: none;">');
+      let $galleryImg = $('<span class="weui-gallery__img">');
+      let $galleryOpr = $('<div class="weui-gallery__opr"><i class="weui-icon-delete weui-icon_gallery-delete"></i></div>')
+      $li.css("background-image", "url(" + img + ")");
+      $galleryImg.css("background-image", "url(" + img + ")");
+      // $files.append($li);
+      $files.parents('.weui-uploader').find('.weui-uploader__files').append($li);
+      $li.append($gallery);
+      $gallery.append($galleryImg);
+      $gallery.append($galleryOpr);
     }
 
     const syncUpload = (localIds, serverIds) => {
@@ -105,7 +115,6 @@ wx.ready( () => {
 
   // 从DOM中删除还未上传的图片，已经上传的图片通过ujs删除的
   $(document).on('click', '.weui-icon_gallery-delete', (e) => {
-    alert('clicked')
     let $li = $(e.target).parents('li.weui-uploader__file')
     if($li.attr('id') == undefined){
       $li.remove();
