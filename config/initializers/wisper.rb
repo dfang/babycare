2c3387e1625d94f44f8f206af9a639488965af9b
havenwei <havenwei82@163.com>
2017-11-22 10:19:45 +0800

    Add auto subscribe to initializers

--- config/initializers/wisper.rb
+++ config/initializers/wisper.rb
@@ -1,9 +1,16 @@
 # frozen_string_literal: true
 
-Wisper.subscribe(ReservationSubscriber.new)
-Wisper.subscribe(UserSubscriber.new)
-Wisper.subscribe(DoctorSubscriber.new)
+# Wisper.subscribe(ReservationSubscriber.new)
+# Wisper.subscribe(UserSubscriber.new)
+# Wisper.subscribe(DoctorSubscriber.new)
 
 # Wisper.subscribe(ImagingExaminationImageSubscriber.new)
 # Wisper.subscribe(MedicalRecordImageSubscriber.new)
 # Wisper.subscribe(LaboratoryExaminationImageSubscriber.new)
+
+filenames = Dir.entries('app/models/subscribers/').select {|f| !File.directory? f}.map { |f| f.gsub!(".rb", "")}
+classes = filenames.map {|f| f.classify.constantize }
+
+classes.each do |c|
+  Wisper.subscribe(c.new)
+end
