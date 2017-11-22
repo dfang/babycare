1612ad5a985417cb87b072eaad3b74257a7ef805
havenwei <havenwei82@163.com>
2017-11-22 10:39:03 +0800

    FIX reservation_*image_subscriber

--- app/models/subscribers/reservation_image_subscriber.rb
+++ app/models/subscribers/reservation_image_subscriber.rb
@@ -1,7 +1,18 @@
 # frozen_string_literal: true
 
 class ReservationImageSubscriber
-  def after_commit(image)
+  def create_reservation_image_successful(image)
+    Rails.logger.info 'create_reservation_image_successful'
+    ProcessWxImageJob.perform_now(image) if image.media_id.present? && image.data.blank?
+  end
+
+  def update_reservation_image_successful(image)
+    Rails.logger.info 'update_reservation_image_successful'
     ProcessWxImageJob.perform_now(image) if image.media_id.present? && image.media_id_changed?
+  end
+
+  def destroy_reservation_image_successful(image)
+    Rails.logger.info 'destroy_reservation_image_successful'
+    # TODO: 从七牛删除该图片
   end
 end
