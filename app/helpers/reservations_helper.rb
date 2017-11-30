c2e6d216c647cba8e119cf42251cf78e7f92e314
havenwei <havenwei82@163.com>
2017-11-30 17:11:17 +0800

    Improve date format with today tomorrow and the day after tomorrow

--- app/helpers/reservations_helper.rb
+++ app/helpers/reservations_helper.rb
@@ -1,5 +1,13 @@
 module ReservationsHelper
   def date_format(date)
-    date.strftime("%Y-%m-%d%P%I:%M").gsub("am", "上午").gsub("pm", "下午")
+    if date.today?
+      date.strftime("今天%P%I:%M").gsub("am", "上午").gsub("pm", "下午")
+    elsif (date - 1.day).today?
+      date.strftime("明天%P%I:%M").gsub("am", "上午").gsub("pm", "下午")
+    elsif (date - 2.day).today?
+      date.strftime("后天%P%I:%M").gsub("am", "上午").gsub("pm", "下午")
+    else
+      date.strftime("%Y-%m-%d%P%I:%M").gsub("am", "上午").gsub("pm", "下午")
+    end
   end
 end
