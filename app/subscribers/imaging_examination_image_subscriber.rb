b04281e2a9ec2e25b7f7be09402f622a43bf3935
fang duan <df1228@gmail.com>
2017-12-02 11:00:41 +0800

    Add app/subscribers

---
+++ app/subscribers/imaging_examination_image_subscriber.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class ImagingExaminationImageSubscriber
+  # def after_commit(image)
+  #   ProcessWxImageJob.perform_now(image) if image.media_id.present? && image.media_id_changed?
+  # end
+end
