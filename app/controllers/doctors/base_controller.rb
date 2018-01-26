e52f14f51b42622fcd3238517727550122867be4
fang duan <df1228@gmail.com>
2018-01-26 13:51:56 +0800

    Fix

--- app/controllers/doctors/base_controller.rb
+++ app/controllers/doctors/base_controller.rb
@@ -1,11 +1,11 @@
 class Doctors::BaseController < InheritedResources::Base
   before_action :authenticate_user!
-  before_action :check_is_verified_doctor
+  before_action :check_legality
 
 
   protected
 
-  def check_is_verified_doctor
-    redirect_to(doctors_status_path) unless current_user.verified_doctor?
+  def check_legality
+    redirect_to doctors_status_path unless current_user.has_valid_contracts?
   end
 end
