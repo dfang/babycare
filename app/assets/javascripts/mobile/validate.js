2f8e9bf87a86b67809e76925559854bdd71eab1c
fang duan <df1228@gmail.com>
2018-01-26 10:03:12 +0800

    Fix doctor career page validation

---
+++ app/assets/javascripts/mobile/validate.js
@@ -0,0 +1,14 @@
+$(function(){
+  console.log('validate ....')
+  $("form.parsley-validate").parsley({ uiEnabled: true, errorsWrapper: "" })
+      .on("field:error", function() {
+        $(this.$element)
+          .parents(".weui-cell")
+          .addClass("validation_error");
+      })
+      .on("field:success", function() {
+        $(this.$element)
+          .parents(".weui-cell")
+          .removeClass("validation_error");
+      });
+})
