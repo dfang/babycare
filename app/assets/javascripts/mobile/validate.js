9c3b12c6459eb57b9ca6c4e3f7d778e9ef54d64b
fang duan <df1228@gmail.com>
2018-01-26 10:42:55 +0800

    Fix validatation on non simple_form page

--- app/assets/javascripts/mobile/validate.js
+++ app/assets/javascripts/mobile/validate.js
@@ -1,12 +1,10 @@
 $(function(){
   console.log('validate ....')
-  $("form.parsley-validate").parsley({ uiEnabled: true, errorsWrapper: "" })
-      .on("field:error", function() {
+  $("form.parsley-validate").length > 0 && $("form.parsley-validate").parsley({ uiEnabled: true, errorsWrapper: "" }).on("field:error", function() {
         $(this.$element)
           .parents(".weui-cell")
           .addClass("validation_error");
-      })
-      .on("field:success", function() {
+      }).on("field:success", function() {
         $(this.$element)
           .parents(".weui-cell")
           .removeClass("validation_error");
