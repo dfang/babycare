1d09ab5fbe5d46009c01e2f2dd926f844b4a3272
fang duan <df1228@gmail.com>
2018-02-02 17:56:02 +0800

    Fix

--- app/controllers/doctors/base_controller.rb
+++ app/controllers/doctors/base_controller.rb
@@ -1,11 +1,15 @@
 class Doctors::BaseController < InheritedResources::Base
+  helper_method :current_doctor
   before_action :authenticate_user!
   before_action :check_legality
-
 
   protected
 
   def check_legality
-    redirect_to doctors_status_path unless current_user.has_valid_contracts?
+    redirect_to doctors_status_path unless current_docotr && current_docotr.has_valid_contracts?
+  end
+
+  def current_doctor
+    @current_doctor ||= current_user.doctor if current_user.doctor.present?
   end
 end
